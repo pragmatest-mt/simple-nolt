0aae81650730cd16c7288ac3ba1f62963f4e5f1f
Andrea <dre086@gmail.com>
2022-10-19 01:13:50 +0200

    feat: Adding GET order endpoint

--- nolt-customer-orders/src/main/java/com/pragmatest/nolt/customer_orders/web/controllers/CustomerServiceController.java
+++ nolt-customer-orders/src/main/java/com/pragmatest/nolt/customer_orders/web/controllers/CustomerServiceController.java
@@ -3,11 +3,13 @@ package com.pragmatest.nolt.customer_orders.web.controllers;
 import com.pragmatest.nolt.customer_orders.services.CustomerOrdersService;
 import com.pragmatest.nolt.customer_orders.services.models.Order;
 import com.pragmatest.nolt.customer_orders.web.controllers.requests.SubmitOrderRequest;
+import com.pragmatest.nolt.customer_orders.web.controllers.responses.GetOrderResponse;
 import com.pragmatest.nolt.customer_orders.web.controllers.responses.SubmitOrderResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,21 +22,26 @@ public class CustomerServiceController {
     ModelMapper mapper;
 
     @PostMapping(value = "orders", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseStatus(HttpStatus.CREATED)
-    public SubmitOrderResponse submit(@RequestHeader(name = "X-Customer-Id") String customerId, @RequestBody SubmitOrderRequest request) {
+    public ResponseEntity<SubmitOrderResponse> submit(@RequestHeader(name = "X-Customer-Id") String customerId, @RequestBody SubmitOrderRequest request) {
 
         Order orderSubmission = mapper.map(request, Order.class);
         orderSubmission.setCustomerId(customerId);
 
         String orderId = customerOrdersService.submitOrder(orderSubmission);
-        return new SubmitOrderResponse(orderId);
+
+        return ResponseEntity.ok(new SubmitOrderResponse(orderId));
     }
 
-    //TODO 1. Add a method to expose a new GET endpoint to retrieve details of an existing order.
-    //Hints:
-    // - the order id can be passed as a path variable in the GET request.
-    // - this endpoint returns information related to the specific order and customer details.
-    // - annotation is -> @GetMapping(value = "orders/{orderId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "orders/{orderId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<GetOrderResponse> get(@RequestHeader(name = "X-Customer-Id") String customerId, @PathVariable String orderId) {
 
+        Order order = customerOrdersService.getOrder(orderId, customerId);
 
+        if (order == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        GetOrderResponse getOrderResponse = mapper.map(order, GetOrderResponse.class);
+        return ResponseEntity.ok(getOrderResponse);
+    }
 }
